3a7394137577a3779ee1d2a352b3c0361f9e169f
TheInfiniter <dimonus98@yandex.ru>
2021-02-14 17:28:59 +0300

    не доделал амплитудную модуляцию (может и не надо будет?)

--- Harmonic/Harmonic/Modulation/AmplitudeKeyShifting.cs
+++ Harmonic/Harmonic/Modulation/AmplitudeKeyShifting.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using OxyPlot;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,6 +9,15 @@ namespace Harmonic.Modulation
 {
     public class AmplitudeKeyShifting
     {
+        public AmplitudeKeyShifting()
+        {
 
+        }
+
+        public DataPoint GeneratePoint()
+        {
+            DataPoint point = new DataPoint();
+            return point;
+        }
     }
 }
